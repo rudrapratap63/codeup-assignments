6e4827cf7799f7b5230d0871f29a6a6e735e24e2
Rudra Pratap Singh <130541940+rudrapratap63@users.noreply.github.com>
2024-09-03 01:29:06 +0530

    Completed Task 5

---
+++ javaAssignment_Task5.java
@@ -0,0 +1,25 @@
+    import java.util.*;
+    public class javaAssignment_Task5 {
+        static int solve(String s) {
+            HashMap<Character,Integer> mpp = new HashMap<Character, Integer> ();
+    
+            int left = 0, right = 0;
+            int n = s.length();
+            int len = 0;
+            while (right<n) {
+                if(mpp.containsKey(s.charAt(right))) {
+                    left = Math.max(mpp.get(s.charAt(right))+1,left);
+                }
+                mpp.put(s.charAt(right), right);
+                len = Math.max(len, right - left + 1);
+                right++;
+            }
+            return len;
+        }
+    
+        public static void main(String args[]) {
+            Scanner sc = new Scanner(System.in);     
+            String str=sc.nextLine();
+            System.out.println(solve(str));
+}
+    }
