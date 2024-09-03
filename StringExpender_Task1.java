30c594018a73acc13450e545989a3137816db598
Rudra Pratap Singh <130541940+rudrapratap63@users.noreply.github.com>
2024-09-03 16:06:50 +0530

    Update StringExpender_Task1.java

--- StringExpender_Task1.java
+++ StringExpender_Task1.java
@@ -10,39 +10,60 @@
  */
 
 import java.util.Scanner;
-
 public class StringExpender_Task1 {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter the input : ");
-        String str = input.nextLine();
-
-        if(str.length()>100){
-            System.out.println("Error: please check input length , input length does not exceed 100 char");
+    static void verify(String str) {
+        int count = 0;
+        for(int i = 0; i<str.length(); i++) {
+            if(Character.isDigit(str.charAt(i))) {
+                count++;
+            }
         }
-        StringBuilder result = new StringBuilder(); 
-        
+        if(count==0) {
+            System.out.println("No integer Found to catch frequency");
+        }
+        else{
+            func(str.toLowerCase());
+        }
+    }	
+    static void func(String str){
+        StringBuilder result = new StringBuilder();
         for (int i = 0; i < str.length(); i++) {
             char currentChar = str.charAt(i);
             if (Character.isAlphabetic(currentChar)) {
-                i++; 
-                int count = 0;
-
+                i++;
+                int frequency = 0;
                 while (i < str.length() && Character.isDigit(str.charAt(i))) {
-                    count = count * 10 + (str.charAt(i) - '0'); 
+                    frequency = frequency*10+(str.charAt(i) - '0'); 
                     i++;
                 }
-                i--; 
-
-                for (int k = 0; k < count; k++) {
+                i--;
+                for (int k = 0; k < frequency; k++) {
                     result.append(currentChar);
                 }
             }
         }
-
-        // final result
-        System.out.println("The result output is : ");
+        System.out.println("The result of the given input is: ");
         System.out.println(result.toString());
-        input.close();
     }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+//        int cases;
+//        System.out.println(constant.ENTER_NUMBER);
+//        int number = sc.nextInt();
+        int count=3;
+        while(count>0) {
+            System.out.println("Enter a input string");
+            String str = input.nextLine();
+            if(str.length()>100){
+                System.out.println("String length not be exceed 100 characters");
+            }
+            else
+            {
+                verify(str);
+                count--;
+            }      
+        }
+
+        input.close();
+}
 }
